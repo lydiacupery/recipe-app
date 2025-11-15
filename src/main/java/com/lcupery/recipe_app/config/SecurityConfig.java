ac1208754b5b3e4397d79fdcf8c17dd6f091e2c8
Lydia Cupery <lydiacpry@gmail.com>
2025-11-15 07:15:15 -0500

    update cors

--- src/main/java/com/lcupery/recipe_app/config/SecurityConfig.java
+++ src/main/java/com/lcupery/recipe_app/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
         http.cors().and().csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("OPTIONS", "/api/**").permitAll()  // Allow CORS preflight requests
                         .requestMatchers("/api/recipes/**").authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
